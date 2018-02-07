6e83202c863c67ccb2c3f15e379769c086e5c39c
Fiona Ho <fh2347@columbia.edu>
2018-02-06 22:18:49 -0500

    clock

--- A2.2/CLOCK 1.js
+++ A2.2/CLOCK 1.js
@@ -2,16 +2,17 @@ function setup() {
   createCanvas(1000, 1000);
 }
 function draw() {
-	background('orange') ;
+	background('white') ;
 
-	fill('pink')
-
-	rect(20,20,map(second(), 0, 60, 0, width), 60);
-	rect(20,80,map(minute(), 0, 60, 0, width), 60);
-
+push();
 	fill(0,0,0);
-	ellipse(250, 300, map(minute(), 0, 60, 0, 600), map(hour(), 0, 24, 0, 400));  
+	ellipse(50, 50, map(second(), 0, 60, 0, 600), map(hour(), 0, 24, 0, 400));  
+pop();
 
-
+	fill('grey')
+push();
+translate(map(minute(), 0, 60, 0, 900), 20);
+rect(0, 0, 55, 55); // Draw rect at new 0,0
+pop();
 
 }
