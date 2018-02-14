1b7bd8654c597a0e5dd3e4a63e3c545b94778f87
Fiona Ho <fh2347@columbia.edu>
2018-02-13 22:30:31 -0500

    Text

---
+++ A3.1/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(640, 480);
+}
+
+function draw() {
+	background('grey');
+    fill(255, 204, 0);
+    rect(20, 20, 60, 60);
+    ellipse(56, 46, 55, 55);
+    textSize(60);
+    fill(0, 102, 153);
+    text('WHAT IS HAPPENING', 10, 60);
+  }
